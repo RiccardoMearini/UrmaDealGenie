3f048df316a62bb992c47c64cc8e7b89600ffb71
James Boswell <bosweljj@utcain.com>
2021-11-29 23:47:50 +0000

    Update build script commands (not really ps1!!)

--- build/build.ps1
+++ build/build.ps1
@@ -3,8 +3,12 @@ dotnet nuget add source https://api.nuget.org/v3/index.json -n nuget.org
 dotnet tool install --global Amazon.Lambda.Tools
 
 dotnet restore ./src/UrmaDealGenie
-dotnet lambda package -pl ./src/UrmaDealGenie
+dotnet lambda package UrmaDealGenieAWS-beta-2.0.zip -pl ./src/UrmaDealGenie
 
 dotnet restore ./src/UrmaDealGenieApp
 dotnet publish ./src/UrmaDealGenieApp -c Release -o ./src/UrmaDealGenieApp/publish
+zip UrmaDealGenieApp-beta-2.0.zip ./src/UrmaDealGenieApp/publish/
+
+# Windows zip app
 Compress-Archive -Update ./src/UrmaDealGenieApp/publish/* UrmaDealGenieApp.zip
+
