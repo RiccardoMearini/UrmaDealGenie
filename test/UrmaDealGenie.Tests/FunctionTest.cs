a1765f25404ab541c95ff76d51b985ab2b7765d6
James Boswell <bosweljj@utcain.com>
2021-11-30 19:05:27 +0000

    Run single test

--- test/UrmaDealGenie.Tests/FunctionTest.cs
+++ test/UrmaDealGenie.Tests/FunctionTest.cs
@@ -2,27 +2,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 using Xunit;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.TestUtilities;
 
 using UrmaDealGenie;
+using System.IO;
 
 namespace UrmaDealGenie.Tests
 {
     public class FunctionTest
     {
         [Fact]
-        public async void TestToUpperFunction()
+        public async void TestFunctionCall()
         {
 
             // Invoke the lambda function and confirm the string was upper cased.
             var function = new Function();
             var context = new TestLambdaContext();
-            var dealResponses = await function.FunctionHandler(new DealRuleSet(), context);
+            var text = File.ReadAllText("dealrules.json");
+            Console.WriteLine($"{text}");
+            var input = JsonSerializer.Deserialize<DealRuleSet>(text);
+            var dealResponses = await function.FunctionHandler(input, context);
 
-            Assert.Equal(2, dealResponses.Count);
+            Assert.Equal(5, dealResponses.Count);
         }
     }
 }
