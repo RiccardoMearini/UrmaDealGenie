cfd217c6f15601d16b29a3670381c78fabdb1cf6
James Boswell <bosweljj@utcain.com>
2021-11-30 08:51:00 +0000

    Build beta 2.1

--- build/build.ps1
+++ build/build.ps1
@@ -3,13 +3,13 @@ dotnet nuget add source https://api.nuget.org/v3/index.json -n nuget.org
 dotnet tool install --global Amazon.Lambda.Tools
 
 dotnet restore ./src/UrmaDealGenie
-dotnet lambda package UrmaDealGenieAWS-beta-2.0.zip -pl ./src/UrmaDealGenie
+dotnet lambda package UrmaDealGenieAWS-beta-2.1.zip -pl ./src/UrmaDealGenie
 
 dotnet restore ./src/UrmaDealGenieApp
 dotnet publish ./src/UrmaDealGenieApp -c Release -o ./src/UrmaDealGenieApp/publish
 
 # Sort this out so that it unzips to a folder called UrmaDealGenieApp
-zip UrmaDealGenieApp-beta-2.0.zip ./src/UrmaDealGenieApp/publish/*
+zip UrmaDealGenieApp-beta-2.1.zip ./src/UrmaDealGenieApp/publish/*
 
 # Windows zip app
 Compress-Archive -Update ./src/UrmaDealGenieApp/publish/* UrmaDealGenieApp.zip
