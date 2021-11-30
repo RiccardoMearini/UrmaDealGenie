938280e7c73c9cea2a4a3c9f41399ef0cb888857
James Boswell <bosweljj@utcain.com>
2021-11-30 13:06:09 +0000

    Complete build of lambda, app & docker image

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,5 @@
-# Run this with: source ./build/build.sh
+# Edit 'ver' and run this with from solution root:
+# source ./build/build.sh
 
 export ver=beta-2.1
 
@@ -8,6 +9,15 @@ dotnet lambda package UrmaDealGenieAWS-${ver}.zip -pl ./src/UrmaDealGenie
 
 # Build .NET Core Application Package
 dotnet restore ./src/UrmaDealGenieApp
-dotnet publish ./src/UrmaDealGenieApp -c Release -o ./UrmaDealGenieApp
-zip UrmaDealGenieApp-${ver}.zip ./UrmaDealGenieApp/*
-rm -r ./UrmaDealGenieApp
+dotnet publish ./src/UrmaDealGenieApp -c Release -o ./src/UrmaDealGenieApp/UrmaDealGenieApp
+cd  ./src/UrmaDealGenieApp
+zip ../../UrmaDealGenieApp-${ver}.zip ./UrmaDealGenieApp/*
+
+# Build docker image and push to registry
+docker build -t urmagurd/deal-genie:${ver} -f Dockerfile .
+docker push urmagurd/deal-genie:${ver}
+
+cd ../..
+
+# Remove temporary app publish folder
+rm -r ./src/UrmaDealGenieApp/UrmaDealGenieApp
