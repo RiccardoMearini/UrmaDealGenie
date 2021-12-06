8aea06d77172dff9e491681fc849ef22dfdf5fab
James Boswell <bosweljj@utcain.com>
2021-12-06 15:04:29 +0000

    Build

--- build/build.sh
+++ build/build.sh
@@ -1,12 +1,13 @@
 # Set 'ver' env variable, and run this command with from solution root:
 #   source ./build/build.sh
 
+# Restore all project dependencies
+dotnet restore
+
 # Build AWS Package
-dotnet restore ./src/UrmaDealGenie
 dotnet lambda package UrmaDealGenieAWS-${ver}.zip -pl ./src/UrmaDealGenie
 
 # Build .NET Core Application Package
-dotnet restore ./src/UrmaDealGenieApp
 dotnet publish ./src/UrmaDealGenieApp -c Release -o ./src/UrmaDealGenieApp/UrmaDealGenieApp
 cd  ./src/UrmaDealGenieApp
 zip ../../UrmaDealGenieApp-${ver}.zip ./UrmaDealGenieApp/*
@@ -19,3 +20,11 @@ cd ../..
 
 # Remove temporary app publish folder
 rm -r ./src/UrmaDealGenieApp/UrmaDealGenieApp
+
+###########################################
+# Publish single file .NET Core Application
+dotnet publish ./src/UrmaDealGenieApp -c Release \
+  -o ./src/UrmaDealGenieApp/UrmaDealGenieApp \
+  -p:PublishSingleFile=true --self-contained false \
+  -r win10-x64 
+
