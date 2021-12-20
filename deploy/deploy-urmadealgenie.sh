620842a865ba83f09e7abb04334af86e5381f63e
James Boswell <bosweljj@utcain.com>
2021-12-20 13:26:33 +0000

    parameterised dealrule

--- deploy/deploy-urmadealgenie.sh
+++ deploy/deploy-urmadealgenie.sh
@@ -1,5 +1,5 @@
 aws cloudformation deploy \
   --template-file deploy-urmadealgenie.yml \
   --stack-name urma-deal-genie \
-  --parameter-overrides file://apikey.json \
+  --parameter-overrides file://parameters.json \
   --capabilities CAPABILITY_NAMED_IAM --region eu-west-1
