7681a83fe6d55715e90b01b8c2ceb18719684b21
James Boswell <bosweljj@utcain.com>
2021-12-31 15:44:36 +0000

    Uncomment deploy commands

--- deploy/deploy-urmadealgenie.sh
+++ deploy/deploy-urmadealgenie.sh
@@ -7,9 +7,9 @@ aws s3 cp deploy-urmadealgenie.yml s3://urmagurd/
 aws s3 cp ../src/UrmaDealGenieApp/dealrules.json s3://urmagurd/
 
 # Deploy package from S3 with CloudFormation
-# aws cloudformation deploy --region eu-west-1 --capabilities CAPABILITY_NAMED_IAM \
-#   --stack-name urma-deal-genie \
-#   --template-file deploy-urmadealgenie.yml \
-#   --s3-bucket urmagurd \
-#   --parameter-overrides file://parameters.json \
+aws cloudformation deploy --region eu-west-1 --capabilities CAPABILITY_NAMED_IAM \
+  --stack-name urma-deal-genie \
+  --template-file deploy-urmadealgenie.yml \
+  --s3-bucket urmagurd \
+  --parameter-overrides file://parameters.json \
   
