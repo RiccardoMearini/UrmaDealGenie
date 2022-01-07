3efe115e4a7b97f4a366dba628fec8229ec7a6b0
James Boswell <bosweljj@utcain.com>
2022-01-07 18:30:25 +0000

    Deploy script delete existing stack

--- deploy/deploy-urmadealgenie.sh
+++ deploy/deploy-urmadealgenie.sh
@@ -1,10 +1,12 @@
-# Build lambda package
+# Build lambda package (from ./deploy folder)
 dotnet lambda package UrmaDealGenieAWS-localbuild.zip -pl ../src/UrmaDealGenie
 
 # Upload lambda package to S3
 aws s3 cp UrmaDealGenieAWS-localbuild.zip s3://urmagurd/
 aws s3 cp deploy-urmadealgenie.yml s3://urmagurd/
 aws s3 cp ../src/UrmaDealGenieApp/dealrules.json s3://urmagurd/
+
+aws cloudformation delete-stack --stack-name urma-deal-genie
 
 # Deploy package from S3 with CloudFormation
 aws cloudformation deploy --region eu-west-1 --capabilities CAPABILITY_NAMED_IAM \
